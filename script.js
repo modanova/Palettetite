49f60e986b558c2a2b1b48371a7a21267c13bcb8
Mo Danova <99407460+modanova@users.noreply.github.com>
2022-05-30 23:24:15 +0100

    functions fixed

--- script.js
+++ script.js
@@ -1,67 +1,58 @@
 const columnsObj = document.getElementsByClassName('column');
-const columnArr = Object.values(document.getElementsByClassName('column'))
+const columnArr = Object.values(document.getElementsByClassName('column'));
+var currPalette;
 applyRandomPalette();
 
-// Generate random HSL values
-function randomHslValues() {
+// Generate random HSL color
+function randomHslColor() {
   return [Math.round(Math.random() * 360), Math.round(Math.random() * 50 + 50), Math.round(Math.random() * 50 + 50)];
 }
 
 // Return HSL format from values array
-function valToHsl(values) {
+function toHslFormat(values) {
   return `hsl(${values[0]}, ${values[1]}%, ${values[2]}%)`;
 }
 
 // Apply a random palette to all columns
 function applyRandomPalette() {
-  for (c of columnsObj) {c.style.backgroundColor = valToHsl(randomHslValues())}
+  for (c of columnsObj) {c.style.backgroundColor = toHslFormat(randomHslColor())}
 }
 
 // Generate a complementary colour from values array
 function complemHsl(values) {
   let hue = values[0];
-  if (hue >= 180) {
-    hue = hue - 180;
-  }
-  else if (hue < 180){
-    hue = hue + 180;
-  }
-  values[0] = hue;
-  return values;
+  hue = (hue + 180) % 360;
+  return [hue, values[1], values[2]];
 }
 
 // Modify values' second in thirds value representing saturation and lightness
 function randomSL(values) {
-  values[1] = Math.round(Math.random() * 100);
-  values[2] = Math.round(Math.random() * 100);
-  return values;
+  return [values[0], Math.round(Math.random() * 100), Math.round(Math.random() * 100)];
 }
 
 // Generate a complementary palette array
 function complemPalette() {
   let palette = [];
-  let val = [];
-  val = randomHslValues();
-  palette.push(valToHsl(val));
-  let valCmpl = [];
-  valCmpl = complemHsl(val);
-  palette.push(valToHsl(valCmpl));
-  palette.push(valToHsl(randomSL(val)));
-  palette.push(valToHsl(randomSL(val)));
-  palette.push(valToHsl(randomSL(valCmpl)));
+  let val = randomHslColor();
+  palette.push(val);
+  let valCmpl = complemHsl(val);
+  palette.push(valCmpl);
+  palette.push(randomSL(val));
+  palette.push(randomSL(val));
+  palette.push(randomSL(valCmpl));
   return palette;
 }
 
 // Apply complementary palette to all columns
 function applyComplemPalette() {
-  let palette = complemPalette();
+  currPalette = complemPalette();
   for (let i = 0; i < 5; i++) {
-    columnArr[i].style.backgroundColor = palette[i];
+    columnArr[i].style.backgroundColor = toHslFormat(currPalette[i]);
   }
 }
 
 // Swap individual element's colour
 function swapOne(el) {
-  el.parentElement.style.backgroundColor=valToHsl(randomHslValues());
+  el.parentElement.style.backgroundColor=toHslFormat(randomHslColor());
   return el;  
 }
