93bd38c9d4d3adbcf2e3917b2e0293e1d8922725
Mo Danova <99407460+modanova@users.noreply.github.com>
2022-05-30 16:42:06 +0100

    Random and complementory color buttons

--- script.js
+++ script.js
@@ -1,26 +1,60 @@
-const columns = document.getElementsByClassName('column');
-randomPalette();
+const columnsObj = document.getElementsByClassName('column');
+const columnArr = Object.values(document.getElementsByClassName('column'))
+applyRandomPalette();
 
+// Generate random HSL values
 function randomHslValues() {
-  return [Math.round(Math.random() * 360), Math.round(Math.random() * 100), Math.round(Math.random() * 100)]
+  return [Math.round(Math.random() * 360), Math.round(Math.random() * 100), Math.round(Math.random() * 100)];
 }
 
-function hslRandom(values) {
+// Return HSL format from values array
+function valToHsl(values) {
   return `hsl(${values[0]}, ${values[1]}%, ${values[2]}%)`;
 }
 
-function randomPalette() {
-  for (c of columns) {c.style.backgroundColor=hslRandom(randomHslValues())}
+// Apply a random palette to all columns
+function applyRandomPalette() {
+  for (c of columnsObj) {c.style.backgroundColor = valToHsl(randomHslValues())}
 }
 
+// Generate a complementary colour from values array
 function complemHsl(values) {
   let hue = values[0];
-  if (hue > 180) {
-    hue -= 180;
+  if (hue >= 180) {
+    hue = hue - 180;
   }
-  else {
-    hue += 180;
+  else if (hue < 180){
+    hue = hue + 180;
   }
   values[0] = hue;
   return values;
 }
+
+// Modify values' second in thirds value representing saturation and lightness
+function randomSL(values) {
+  values[1] = Math.round(Math.random() * 100);
+  values[2] = Math.round(Math.random() * 100);
+  return values;
+}
+
+// Generate a complementary palette array
+function complemPalette() {
+  let palette = [];
+  let val = randomHslValues();
+  palette.push(valToHsl(val));
+  let valCmpl = complemHsl(val);
+  palette.push(valToHsl(valCmpl));
+  palette.push(valToHsl(randomSL(val)));
+  palette.push(valToHsl(randomSL(valCmpl)));
+  palette.push(valToHsl(randomSL(valCmpl)));
+  return palette;
+}
+
+//
+function applyComplemPalette() {
+  let palette = complemPalette();
+  for (let i = 0; i < 5; i++) {
+    columnArr[i].style.backgroundColor = palette[i];
+  }
+}
+
