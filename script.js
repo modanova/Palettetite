5fe1bf5d602b8dfaf47c1834d265e702d7a3223a
Mo Danova <99407460+modanova@users.noreply.github.com>
2022-05-30 13:59:29 +0100

    hsl constructor created

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const column = document.getElementsByClassName('column');
+const columns = document.getElementsByClassName('column');
 
 function getRandomArbitrary(min, max) {
   return Math.random() * (max - min) + min;
@@ -14,9 +14,14 @@ function randomRgbValues() {
     return values;
 }
 
-let myColor = '';
-let mainColorRgb = '';
-let mainColorHex = '';
+function randomHslValues() {
+  return [Math.round(Math.random() * 360), Math.round(Math.random() * 100), Math.round(Math.random() * 100)]
+}
+
+function hslRandom() {
+  let values = randomHslValues();
+  return `hsl(${values[0]}, ${values[1]}%, ${values[2]}%)`;
+}
 
 function rgbConstructor(values) {
   return rgbColor = 'rgb(' + values[0] + ',' + values[1] + ',' + values[2] + ')';
@@ -26,12 +31,32 @@ function hexConsturctor(values) {
   return `#${values.map(x => x.toString(16)).join("").toUpperCase()}`
 }
 
+function complemValues(values) { // Take a column's colour and change return complementary;
+  return values.map(x => 255 - x);
+}
+
+function brightness(values) {
+  let coin = Math.round(Math.random());
+  let increm = Math.round(Math.random() * 255);
+  if (coin == 1) {
+    values = values.map(x => Math.min(x + increm, 255));
+  }
+  else {
+    values = values.map(x => Math.max(x - increm, 0));
+  }
+  return values;
+}
+
+let myColor = '';
+let mainColorRgb = '';
+let mainColorHex = '';
+
 myColor = randomRgbValues();
 mainColorRgb = 'rgb(' + myColor[0] + ',' + myColor[1] + ',' + myColor[2] + ')';
 mainColorHex = `#${myColor.map(x => x.toString(16)).join("").toUpperCase()}`;
 
-column[0].style.backgroundColor = mainColorHex;
-console.log(column[0].style.backgroundColor);
+columns[0].style.backgroundColor = hexConsturctor(randomRgbValues());
+console.log(columns[0].style.backgroundColor);
 
 let secondColor = [];
 let secondColorRgb = '';
@@ -42,5 +67,5 @@ secondColorRgb = 'rgb(' + secondColor[0] + ',' + secondColor[1] + ',' + secondCo
 secondColorHex = `#${secondColor.map(x => x.toString(16)).join("").toUpperCase()}`;
 
 
-column[1].style.backgroundColor = secondColorHex;
-console.log(column[1].style.backgroundColor);
+columns[1].style.backgroundColor = secondColorHex;
+console.log(columns[1].style.backgroundColor);
