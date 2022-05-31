53e949c2655695925c29c9eca028a9c5ffb6dd7e
Mo Danova <99407460+modanova@users.noreply.github.com>
2022-05-31 15:23:36 +0100

    Display colours

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const columnsObj = document.getElementsByClassName('column');
 const columnArr = Object.values(document.getElementsByClassName('column'));
 var currPalette;
-applyRandomPalette();
+applyPalette(generateRandom());
 
 // Generate random HSL color
 function randomHslColor() {
@@ -13,9 +13,34 @@ function toHslFormat(values) {
   return `hsl(${values[0]}, ${values[1]}%, ${values[2]}%)`;
 }
 
-// Apply a random palette to all columns
-function applyRandomPalette() {
-  for (c of columnsObj) {c.style.backgroundColor = toHslFormat(randomHslColor())}
+function toHexFormat(values) {
+  let [h, s, l] = values;
+  let [a, b, c] = hslToHex(h, s, l);
+  return `#${a,b,c}`;                 // NOT CORRECT
+}
+
+function hslToHex([h, s, l]) {
+  l /= 100;
+  const a = s * Math.min(l, 1 - l) / 100;
+  const f = n => {
+    const k = (n + h / 30) % 12;
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
+  };
+  return `#${f(0)}${f(8)}${f(4)}`.toUpperCase();
+}
+
+
+// Apply a random palette to all columns, display in hex
+
+
+// Apply palette to all columns
+function applyPalette(paletteGen) {
+  currPalette = paletteGen;
+  for (let i = 0; i < 5; i++) {
+    columnArr[i].style.backgroundColor = toHslFormat(currPalette[i]);
+    columnArr[i].querySelector('.color-value').innerHTML = hslToHex(currPalette[i]);
+  }
 }
 
 // Generate a complementary colour from values array
@@ -29,9 +54,14 @@ function complemHsl(values) {
 function randomSL(values) {
   return [values[0], Math.round(Math.random() * 100), Math.round(Math.random() * 100)];
 }
+// Generate a random palette array
+function generateRandom() {
+  let palette = new Array(5).fill(true).map(x => randomHslColor());
+  return palette;
+}
 
 // Generate a complementary palette array
-function complemPalette() {
+function generateComplementary() {
   let palette = [];
   let val = randomHslColor();
   palette.push(val);
@@ -41,14 +71,6 @@ function complemPalette() {
   palette.push(randomSL(val));
   palette.push(randomSL(valCmpl));
   return palette;
-}
-
-// Apply complementary palette to all columns
-function applyComplemPalette() {
-  currPalette = complemPalette();
-  for (let i = 0; i < 5; i++) {
-    columnArr[i].style.backgroundColor = toHslFormat(currPalette[i]);
-  }
 }
 
 // Swap individual element's colour
